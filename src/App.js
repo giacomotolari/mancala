11c46b7dae500853eed6c002d62e17c93fc010b0
giacomotolari <tolarigiacomo@gmail.com>
2021-07-26 14:46:01 +0200

    add style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p>MANCALA</p>
+      <div className='board'>
+        <div className='player1Points'></div>
+        <div className='containers'>
+          <div className='player1Containers'>
+            <div className='container' id='player1Container1'>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+            </div>
+            <div className='container' id='player1Container2'>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+            </div>
+            <div className='container' id='player1Container3'>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+            </div>
+            <div className='container' id='player1Container4'>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+            </div>
+            <div className='container' id='player1Container5'>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+            </div>
+            <div className='container' id='player1Container6'>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+            </div>
+          </div>
+          <div className='player2Containers'>
+            <div className='container' id='player2Container1'>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+            </div>
+            <div className='container' id='player2Container2'>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+            </div>
+            <div className='container' id='player2Container3'>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+            </div>
+            <div className='container' id='player2Container4'>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+            </div>
+            <div className='container' id='player2Container5'>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+            </div>
+            <div className='container' id='player2Container6'>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+              <div className='pebble'></div>
+            </div>
+          </div>
+        </div>
+        <div className='player2Points'></div>
+      </div>
     </div>
   );
 }
