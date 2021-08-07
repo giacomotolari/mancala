8101c0971fc3758702877f87213a526fd70a27eb
giacomotolari <tolarigiacomo@gmail.com>
2021-08-07 13:21:59 +0200

    add props

--- src/Container.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-
-const container = () => {
-  return <div className='container' id='player1Container1'><p>{4}</p></div>;
-};
-
-export default container;
---
+++ src/components/Container.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const container = (props) => {
+  return (
+    <div className='container' onClick={props.containerFunction}>
+      {props.count}
+    </div>
+  );
+};
+
+export default container;
